94047d717c3318769932c608bd78aa99cd9a4c2a
Shreya-Battula <2012047@apps.nsd.org>
2025-11-29 03:55:14 +0000

    Done - need to do graphs/analysis

--- R_Prog_Experiment.r
+++ R_Prog_Experiment.r
@@ -1,7 +1,7 @@
+# Install packages
 install.packages(c("tidyverse", "ggridges", "patchwork", 
                    "viridis", "gapminder", "scales", "ggplot2", 
-                   "tidytext", "text", "tm", "SnowballC", "wordcloud", 
-                   "RColorBrewer"))
+                   "tidytext", "text", "tm"))
 library(tidyverse)
 library(ggridges)
 library(patchwork)
@@ -12,14 +12,12 @@ library(ggplot2)
 library(tidytext)
 library(text)
 library(tm)
-library(SnowballC)
-library(wordcloud)
-library(RColorBrewer)
+
 # Import data and handle missing values - NA
 tourism_data_original <- read.csv("tourism_customer_reviews.csv")
 tourism_data <- na.omit(tourism_data_original)
 
-#----------------------------
+# Visualize Data - Average rating vs location
 avg_rating <- mean(tourism_data$rating)
 print(tourism_data |>
         group_by(location) |>
@@ -29,7 +27,7 @@ print(tourism_data |>
         geom_col(fill = "#c2e5eb") +
         labs(title = "Location vs Rating", x = "Location", y = "Rating"))
 
-#----------------------------
+# Split data by bad, mid, and good reviews
 tourism_data <- tourism_data |>
   mutate(
     Category = case_when(
@@ -43,6 +41,7 @@ bad_reviews_original <- tourism_data |> filter(Category == "Bad")
 mid_reviews_original <- tourism_data |> filter(Category == "Mid")
 good_reviews_original <- tourism_data |> filter(Category == "Good")
 
+# Cleaning: Remove punctuation and change to all lowercase
 bad_reviews <- tolower(gsub("[[:punct:]]", 
                             "", bad_reviews_original$review_text))
 mid_reviews <- tolower(gsub("[[:punct:]]", 
@@ -50,4 +49,16 @@ mid_reviews <- tolower(gsub("[[:punct:]]",
 good_reviews <- tolower(gsub("[[:punct:]]", 
                              "", good_reviews_original$review_text))
 
-#---------------------------------------
+# Create and combine individual category data frames 
+bad_reviews_df <- data.frame(Category = "Bad", text = bad_reviews)
+mid_reviews_df <- data.frame(Category = "Mid", text = mid_reviews)
+good_reviews_df <- data.frame(Category = "Good", text = good_reviews)
+reviews_df <- bind_rows(bad_reviews_df, mid_reviews_df, good_reviews_df)
+
+# Remove stop words aka filler/unnecessary words and additional spaces
+reviews_cleaned <- Corpus(VectorSource(reviews_df$text))
+reviews_cleaned <- tm_map(reviews_cleaned, removeWords, stopwords("english"))
+reviews_cleaned <- sapply(reviews_cleaned, as.character)
+reviews_cleaned <- gsub("\\s+", " ", reviews_cleaned)
+reviews_cleaned <- trimws(reviews_cleaned)
+print(reviews_cleaned)
