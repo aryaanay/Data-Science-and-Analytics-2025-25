a982a496054f847aa11cd8766518e4d72092525a
Shreya-Battula <2012047@apps.nsd.org>
2025-11-30 17:53:14 +0000

    Done

--- R_Prog_Experiment.r
+++ R_Prog_Experiment.r
@@ -1,18 +1,9 @@
 # Install packages
-install.packages(c("tidyverse", "ggridges", "patchwork", 
-                   "viridis", "gapminder", "scales", "ggplot2", 
-                   "tidytext", "text", "tm", "wordcloud"))
+install.packages(c("tidyverse", "text", "tm", "wordcloud"))
 library(tidyverse)
-library(ggridges)
-library(patchwork)
-library(viridis)
-library(gapminder)
-library(scales)
-library(ggplot2)
-library(tidytext)
-library(text)
 library(tm)
 library(wordcloud)
+library(dplyr)
 
 # Import data and handle missing values - NA
 tourism_data_original <- read.csv("tourism_customer_reviews.csv")
@@ -50,8 +41,6 @@ reviews_df <- bind_rows(bad_reviews_df, mid_reviews_df, good_reviews_df)
 
 stop_words <- stopwords::stopwords("en")
 reviews_cleaned <- reviews_df$text
-reviews_cleaned <- tolower(reviews_cleaned)
-reviews_cleaned <- gsub("[[:punct:]]","", reviews_cleaned)
 reviews_cleaned <- gsub("\\s+", " ", reviews_cleaned)
 reviews_cleaned <- trimws(reviews_cleaned)
 
@@ -66,29 +55,34 @@ reviews_cleaned_df <- data.frame(
   text = review_cleaned,
   stringsAsFactors = FALSE
 )
+print(reviews_cleaned_df)
 
 # WORD CLOUDS
 text_bad <- paste(reviews_cleaned_df$text
                   [reviews_cleaned_df$Category == "Bad"], collapse=" ")
 words_bad <- table(unlist(strsplit(text_bad, " ")))
+
+windows(title = "Bad Reviews Word Cloud")
 wordcloud(words = names(words_bad), 
           freq = as.numeric(words_bad), scale = c(4,.5), 
-          min.freq = 3, max.words = Inf, random.order=TRUE, random.color=FALSE, 
+          min.freq = 3, max.words = 30, random.order=FALSE, random.color=FALSE, 
           rot.per=.1, colors="black")
 
 text_mid <- paste(reviews_cleaned_df$text
                   [reviews_cleaned_df$Category == "Mid"], collapse=" ")
 words_mid <- table(unlist(strsplit(text_mid, " ")))
+windows(title = "Mid Reviews Word Cloud")
 wordcloud(words = names(words_mid), 
           freq = as.numeric(words_mid), scale = c(4,.5), 
-          min.freq = 3, max.words = Inf, random.order=TRUE, random.color=FALSE, 
+          min.freq = 3, max.words = 30, random.order=FALSE, random.color=FALSE, 
           rot.per=.1, colors="black")
 
 text_good <- paste(reviews_cleaned_df$text
                    [reviews_cleaned_df$Category == "Good"], collapse=" ")
 words_good <- table(unlist(strsplit(text_good, " ")))
+
+windows(title = "Good Reviews Word Cloud")
 wordcloud(words = names(words_good), 
           freq = as.numeric(words_good), scale = c(4,.5), 
-          min.freq = 3, max.words = Inf, random.order=TRUE, random.color=FALSE, 
+          min.freq = 3, max.words = 30, random.order=FALSE, random.color=FALSE, 
           rot.per=.1, colors="black")
-
